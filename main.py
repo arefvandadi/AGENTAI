00d5a678ad6c8b5b56b50eb4a0b1592cafc580aa
arefvandadi <aref.vandadi@gmail.com>
2024-04-06 19:04:35 -0400

    Import population csv file

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import openai
 openai.api_key = os.getenv('OPENAI_API_KEY_1')
 
+import pandas as pd
+population_path = os.path.join('data', 'WorldPopulation2023.csv')
+population_df = pd.read_csv(population_path)
+print(population_df.head())
+
 
 
 
