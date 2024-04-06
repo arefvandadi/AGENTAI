79d82802aff0af3dabd39ed104650e26ded45da9
arefvandadi <aref.vandadi@gmail.com>
2024-04-06 19:49:11 -0400

    Defining a Query Engine

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ import os
 import openai
 openai.api_key = os.getenv('OPENAI_API_KEY_1')
 
+# Import population csv file 
 import pandas as pd
 population_path = os.path.join('data', 'WorldPopulation2023.csv')
 population_df = pd.read_csv(population_path)
 print(population_df.head())
+
+# Defining a Query Engine
+from llama_index.core.query_engine import PandasQueryEngine
+panda_query_engine = PandasQueryEngine(df=population_df, verbose=True)
+# panda_query_engine.query(
+#     'What is the 34th ranked country based on population?'
+#     )
+
+
 
 
 
