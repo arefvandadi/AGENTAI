0135bb382fdc8866cbfdf295a00a3586fbbcf890
arefvandadi <aref.vandadi@gmail.com>
2024-04-07 11:03:15 -0400

    updated the pdf reader to create indexes and query engine

--- pdf.py
+++ pdf.py
@@ -1,15 +1,34 @@
 import os
-from llama_index.core.readers import SimpleDirectoryReader
+from llama_index.core import SimpleDirectoryReader, load_index_from_storage, VectorStoreIndex, StorageContext, Settings
 from llama_index.readers.file import PDFReader
 from pathlib import Path
+
+# API Key
+import os
+import openai
+openai.api_key = os.getenv('OPENAI_API_KEY_1')
 
 pdf_file_path = os.path.join('data', 'Canada.pdf')
 # Option 1
 # reader = SimpleDirectoryReader(input_files=[pdf_file_path])
-# document1 = reader.load_data()
-# # print(len(document))
+# document = reader.load_data()
 
 # Option 2
 loader = PDFReader()
-document2 = loader.load_data(file=Path(pdf_file_path))
-# print(len(document))
+Canada_pdf_doc = loader.load_data(file=Path(pdf_file_path))
+
+def get_index(data, index_name):
+    index = None
+    if not os.path.exists(index_name):
+        print("building index", index_name)
+        index = VectorStoreIndex.from_documents(data, show_progress=True)
+        index.storage_context.persist(persist_dir=index_name)
+    else:
+        index = load_index_from_storage(
+            StorageContext.from_defaults(persist_dir=index_name)
+        )
+
+    return index
+
+canada_index = get_index(Canada_pdf_doc, "canada")
+canada_engine = canada_index.as_query_engine()
